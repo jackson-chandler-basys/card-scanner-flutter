ee4a5af5e70f67166f78728fc2c09eff8db9a8ff
cjbrown12 <chris.brown@basyspro.com>
2024-10-28 00:28:36 -0500

    Add preprocessImage function

--- android/src/main/kotlin/com/basys/card_scanner/scanner_core/CardScanner.kt
+++ android/src/main/kotlin/com/basys/card_scanner/scanner_core/CardScanner.kt
@@ -16,78 +16,123 @@ import com.basys.card_scanner.onCardScanned
 import com.basys.card_scanner.scanner_core.models.CardDetails
 import com.basys.card_scanner.scanner_core.models.CardScannerOptions
 import com.basys.card_scanner.scanner_core.optimizer.CardDetailsScanOptimizer
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
+import org.opencv.core.Core
 
-class CardScanner(private val scannerOptions: CardScannerOptions, private val onCardScanned: onCardScanned, private val onCardScanFailed: onCardScanFailed) : ImageAnalysis.Analyzer {
-  private val singleFrameCardScanner: SingleFrameCardScanner = SingleFrameCardScanner(scannerOptions)
-  val cardDetailsScanOptimizer: CardDetailsScanOptimizer = CardDetailsScanOptimizer(scannerOptions)
-  private var scanCompleted: Boolean = false
+class CardScanner(
+    private val scannerOptions: CardScannerOptions,
+    private val onCardScanned: onCardScanned,
+    private val onCardScanFailed: onCardScanFailed
+) : ImageAnalysis.Analyzer {
 
-  init {
-    if (scannerOptions.cardScannerTimeOut > 0) {
-      val timer = object : CountDownTimer((scannerOptions.cardScannerTimeOut * 1000).toLong(), 1000) {
-        override fun onTick(millisUntilFinished: Long) {}
+    private val singleFrameCardScanner: SingleFrameCardScanner = SingleFrameCardScanner(scannerOptions)
+    private val cardDetailsScanOptimizer: CardDetailsScanOptimizer = CardDetailsScanOptimizer(scannerOptions)
+    private var scanCompleted: Boolean = false
 
-        override fun onFinish() {
-          debugLog("Card scanner timeout reached", scannerOptions)
-          val cardDetails = cardDetailsScanOptimizer.getOptimalCardDetails()
-          if (cardDetails != null) {
-            finishCardScanning(cardDetails)
-          } else {
-            onCardScanFailed()
-          }
-          debugLog("Finishing card scan with card details : $cardDetails", scannerOptions)
+    init {
+        if (scannerOptions.cardScannerTimeOut > 0) {
+            val timer = object : CountDownTimer((scannerOptions.cardScannerTimeOut * 1000).toLong(), 1000) {
+                override fun onTick(millisUntilFinished: Long) {}
+
+                override fun onFinish() {
+                    debugLog("Card scanner timeout reached", scannerOptions)
+                    val cardDetails = cardDetailsScanOptimizer.getOptimalCardDetails()
+                    if (cardDetails != null) {
+                        finishCardScanning(cardDetails)
+                    } else {
+                        onCardScanFailed()
+                    }
+                    debugLog("Finishing card scan with card details : $cardDetails", scannerOptions)
+                }
+            }
+            timer.start()
         }
-      }
-      timer.start()
     }
-  }
 
-  companion object {
-    private val TAG: String = "TextRecognitionProcess"
-  }
+    companion object {
+        private const val TAG = "TextRecognitionProcess"
+    }
 
-  @OptIn(ExperimentalGetImage::class)
-  @SuppressLint("UnsafeExperimentalUsageError")
-  override fun analyze(imageProxy: ImageProxy) {
-    val mediaImage = imageProxy.image
-    if (mediaImage != null) {
-      val image = InputImage.fromMediaImage(mediaImage, 90)
+    @OptIn(ExperimentalGetImage::class)
+    @SuppressLint("UnsafeExperimentalUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, 90)
+            val preprocessedImage = preprocessImage(image)
 
-      val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+            val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
-      recognizer.process(image)
-              .addOnSuccessListener { visionText ->
-                if (scanCompleted) return@addOnSuccessListener
-                val cardDetails = singleFrameCardScanner.scanSingleFrame(visionText)
+            recognizer.process(preprocessedImage)
+                .addOnSuccessListener { visionText ->
+                    if (scanCompleted) return@addOnSuccessListener
+                    val cardDetails = singleFrameCardScanner.scanSingleFrame(visionText)
                         ?: return@addOnSuccessListener
 
-                if (scannerOptions.enableDebugLogs) {
-                  debugLog("----------------------------------------------------", scannerOptions)
-                  for (block in visionText.textBlocks) {
-                    debugLog("visionText: TextBlock ============================", scannerOptions)
-                    debugLog("visionText : ${block.text}", scannerOptions)
-                  }
-                  debugLog("----------------------------------------------------", scannerOptions)
-
-                  debugLog("Card details : $cardDetails", scannerOptions)
+                    if (scannerOptions.enableDebugLogs) {
+                        debugLog("----------------------------------------------------", scannerOptions)
+                        for (block in visionText.textBlocks) {
+                            debugLog("visionText: TextBlock ============================", scannerOptions)
+                            debugLog("visionText : ${block.text}", scannerOptions)
+                        }
+                        debugLog("----------------------------------------------------", scannerOptions)
+                        debugLog("Card details : $cardDetails", scannerOptions)
+                    }
+                    cardDetailsScanOptimizer.processCardDetails(cardDetails)
+                    if (cardDetailsScanOptimizer.isReadyToFinishScan()) {
+                        finishCardScanning(cardDetailsScanOptimizer.getOptimalCardDetails()!!)
+                    }
                 }
-                cardDetailsScanOptimizer.processCardDetails(cardDetails)
-                if (cardDetailsScanOptimizer.isReadyToFinishScan()) {
-                  finishCardScanning(cardDetailsScanOptimizer.getOptimalCardDetails()!!)
+                .addOnFailureListener { e ->
+                    debugLog("Error : $e", scannerOptions)
                 }
-              }
-              .addOnFailureListener { e ->
-                debugLog("Error : $e", scannerOptions)
-              }
-              .addOnCompleteListener {
-                imageProxy.close()
-              }
+                .addOnCompleteListener {
+                    imageProxy.close()
+                }
+        }
     }
-  }
 
-  private fun finishCardScanning(cardDetails: CardDetails) {
-    debugLog("OPTIMAL Card details : $cardDetails", scannerOptions)
-    scanCompleted = true
-    onCardScanned(cardDetails)
-  }
+    private fun preprocessImage(inputImage: InputImage): InputImage {
+    
+    val bitmap = inputImage.bitmapInternal ?: return inputImage
+    val mat = Mat()
+    Utils.bitmapToMat(bitmap, mat)
+
+    Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2GRAY)
+
+    val kernel = Mat(3, 3, CvType.CV_16S)
+    kernel.put(0, 0, -1.0, -1.0, -1.0, -1.0, 9.0, -1.0, -1.0, -1.0, -1.0)
+    Imgproc.filter2D(mat, mat, mat.depth(), kernel)
+
+    Core.convertScaleAbs(mat, mat, 1.2, 10.0)
+
+    Core.add(mat, Scalar(50.0), mat)
+
+    val mask = Mat(mat.size(), CvType.CV_32F, Scalar(1.0))
+    val center = Point(mat.width() / 2.0, mat.height() / 2.0)
+    val radius = (min(mat.width(), mat.height()) / 2).toDouble()
+    Imgproc.circle(mask, center, radius.toInt(), Scalar(0.0), -1)
+
+    Core.multiply(mat, mask, mat)
+
+    val blurredMat = Mat()
+    Imgproc.GaussianBlur(mat, blurredMat, Size(0.0, 0.0), 10.0)
+    Core.addWeighted(mat, 1.5, blurredMat, -0.5, 0.0, mat)
+
+    val preprocessedBitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888)
+    Utils.matToBitmap(mat, preprocessedBitmap)
+
+    return InputImage.fromBitmap(preprocessedBitmap, inputImage.rotationDegrees)
+}
+
+    private fun finish0CardScanning(cardDetails: CardDetails) {
+        debugLog("OPTIMAL Card details : $cardDetails", scannerOptions)
+        scanCompleted = true
+        onCardScanned(cardDetails)
+    }
 }
